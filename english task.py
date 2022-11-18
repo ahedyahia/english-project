f879bc2b766668f1410e90a0c3330498fb285156
ahedyahia <118531699+ahedyahia@users.noreply.github.com>
2022-11-18 03:40:54 -0800

    Add files via upload

---
+++ english task.py
@@ -0,0 +1,38 @@
+grammer= input ("how does your grammer ")
+for i in range (10000) :
+    if grammer ==  "set side" :
+     print("to keep or safe (sth)for selator")
+     grammer= input ("how does your grammer ")
+    elif grammer =="set back":
+     print("delay")
+     grammer= input ("how does your grammer ")
+    elif grammer == "set down" :
+     print("to stop tolet a passenger got out")
+     grammer= input ("how does your grammer ")
+    elif grammer == "set in" :
+     print("to arrived stay for a period of time escape ally on illness of weather")
+     grammer= input ("how does your grammer ")
+    elif grammer == "set off" :
+     print("to start ajuorney")
+     grammer= input ("how does your grammer ")
+    elif grammer =="set on":
+     print("to attack(sb)suddenly")
+     grammer= input ("how does your grammer ")
+    elif grammer =="judge":
+     print("to form an opinion about (sb) or (sth)")
+     grammer= input ("how does your grammer ")
+    elif grammer =="appearance":
+     print("the way that (sb) or (sth)")
+     grammer= input ("how does your grammer ")
+    elif grammer =="expert":
+     print("(sb)with special skills or knowledge")
+     grammer= input ("how does your grammer ")
+    elif grammer =="word":
+     print("the meaning")
+    elif grammer =="word":
+     print("the meaning")
+    else:
+    
+       print("this word is not definid")
+       grammer= input ("how does your grammer ")
+
